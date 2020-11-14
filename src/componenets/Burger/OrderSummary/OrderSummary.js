e02ae17e42affe44eb9ede846fff690a320a2cdb
Kunal Joshi <kjoshi087@gmail.com>
2020-11-14 16:24:11 +0530

    Restructuring of the components

--- src/componenets/Burger/OrderSummary/OrderSummary.js
+++ src/componenets/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Auxiliary from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
-const orderSummary = (props)=>{
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+    // This can be the functional compnent does not have to be a class based component 
+    componentWillUpdate()
+     {
+         console.log('Order Summary [Will Update]')
+     }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey=>{
         return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]};
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]};
             </li>
         )
     });
-    return (<Auxiliary>
+        return (
+            <Auxiliary>
         <h3>Your Order</h3>
         <p>A delicious burger with the following ingredients</p>
         <ul>
             {ingredientSummary}
         </ul>
-    <p>{props.totalPrice}</p>
+    <p>{this.props.totalPrice}</p>
         <p>Continue to Checkout</p>
-        <Button clicked={props.purchaseCancelled} btnType={"Danger"}>CANCEL</Button>
-        <Button clicked={props.purchaseContinued} btnType={"Success"}>CONTINUE</Button>
+        <Button clicked={this.props.purchaseCancelled} btnType={"Danger"}>CANCEL</Button>
+        <Button clicked={this.props.purchaseContinued} btnType={"Success"}>CONTINUE</Button>
     </Auxiliary>
-    )};
+        )
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/componenets/UI/Modal/Modal.js
+++ src/componenets/UI/Modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import classes from './Modal.module.css'
 import Auxiliary from '../../../hoc/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = (props)=>{
-    return (
-    <Auxiliary>
-    <Backdrop clicked={props.modalClosed} show={props.show}/>
+class Modal extends Component{
+    shouldComponentUpdate(nextProps,nextState)
+    {
+        // Using this we have stopped the unnecessary rendering of the functions.
+        return nextProps.show!==this.props.show;
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] Will Update')
+    }
+    render()
+    {
+        return (
+            <Auxiliary>
+                {/* When this needs to be shown needs to be checked the modalClosed function will be  */}
+    <Backdrop clicked={this.props.modalClosed} show={this.props.show}/>
     <div className={classes.Modal}
         style={{
-            transform: props.show?'translateY(0)':'translateY(-100vh)',
-            opacity: props.show?'1':'0'
+            transform: this.props.show?'translateY(0)':'translateY(-100vh)',
+            opacity: this.props.show?'1':'0'
         }}
     >
-        {props.children}
+        {this.props.children}
     </div>
-    </Auxiliary>)
-};
+    </Auxiliary>
+        )
+    }
+}
 
 export default Modal;
